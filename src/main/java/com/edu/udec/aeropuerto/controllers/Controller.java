4e1a60aee79c14623a96f00b3d48702b02b755cb
Pedrioko <pedro3manjarrez@hotmail.com>
2019-12-14 22:21:10 -0500

    init

--- src/main/java/com/edu/udec/aeropuerto/controllers/Controller.java
+++ src/main/java/com/edu/udec/aeropuerto/controllers/Controller.java
@@ -12,11 +12,13 @@ import com.edu.udec.aeropuerto.controllers.domain.StatusResponse;
 import com.edu.udec.aeropuerto.dao.SensorRepository;
 import com.edu.udec.aeropuerto.domain.Dato;
 import com.edu.udec.aeropuerto.domain.Sensor;
+import com.edu.udec.aeropuerto.domain.enums.RangoRuido;
 import com.edu.udec.aeropuerto.math.FastV;
 import com.google.gson.Gson;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.Version;
+import weka.associations.Apriori;
 import weka.classifiers.functions.LinearRegression;
 import weka.clusterers.SimpleKMeans;
 import weka.core.Attribute;
@@ -53,61 +55,56 @@ public class Controller {
             String params = req.params(":id");
             Optional<Sensor> byId = sensorRepo.findById(Long.valueOf(params));
             List<Dato> historial = byId.get().getHistorial();
-            FastVector atts = new FastVector();
-            atts.addElement(new Attribute("Decibeles", 0));
-            atts.addElement(new Attribute("Clasificacion", 1));
+            if (historial == null || historial.isEmpty())
+                return "{}";
+            else {
+                FastVector atts = new FastVector();
+                atts.addElement(new Attribute("Decibeles", 0));
+                atts.addElement(new Attribute("Clasificacion", 1));
 
-            Instances dataset = new Instances("Dataset", atts, historial.size());
+                Instances dataset = new Instances("Dataset", atts, historial.size());
 
-            historial.forEach(e -> {
-                dataset.add(new Instance(1.0, new double[]{e.getDecibeles(), e.getClasificacionRuido().getValue()}));
-            });
-            SimpleKMeans skm = new SimpleKMeans();
-            skm.buildClusterer(dataset);
-            String cad = "[";
+                historial.forEach(e -> {
+                    dataset.add(new Instance(1.0, new double[]{e.getDecibeles(), e.getClasificacionRuido().getValue()}));
+                });
+                SimpleKMeans skm = new SimpleKMeans();
+                skm.buildClusterer(dataset);
+                String cad = "[";
 
-            Instances cent = skm.getClusterCentroids();
-            for (int i = 0; i < cent.numInstances(); i++) {
-                Instance e = cent.instance(i);
-                int w = e.numValues();
-                cad += "{\"nombre\": \"" + e.attribute(i).name() + "\", \"values\":[";
-                for (int j = 0; j < w; j++) {
-                    cad += e.toString(j) + (j != (w - 1) ? "," : "");
+                Instances cent = skm.getClusterCentroids();
+                for (int i = 0; i < cent.numInstances(); i++) {
+                    Instance e = cent.instance(i);
+                    int w = e.numValues();
+                    cad += "{\"nombre\": \"" + e.attribute(i).name() + "\", \"values\":[";
+                    for (int j = 0; j < w; j++) {
+                        cad += e.toString(j) + (j != (w - 1) ? "," : "");
+                    }
+                    cad += "]},";
                 }
-                cad += "]},";
+
+
+                cad = cad + "]";
+                System.out.println("Skm " + skm);
+                cad = cad.replace("},]", "}]");
+                return cad;
             }
-
-
-            cad = cad + "]";
-            System.out.println("Skm " + skm);
-            cad = cad.replace("},]", "}]");
-            return cad;
         });
         get("/api/sensor/:id/regresion", (req, res) -> {
             String params = req.params(":id");
             Optional<Sensor> byId = sensorRepo.findById(Long.valueOf(params));
+
             List<Dato> historial = byId.get().getHistorial();
-/*            FastVector atts = new FastVector();
-            Attribute fechaCreacion = new Attribute("FechaCreacion");
-            atts.addElement(fechaCreacion);
-            atts.addElement(new Attribute("Decibeles"));
+            if (historial == null || historial.isEmpty())
+                return "{}";
+            else {
+                FastV fv = new FastV(historial.size());
 
-            Instances dataset = new Instances("Dataset", atts, historial.size());
-            dataset.setClassIndex(dataset.numAttributes() - 1);
-
-            historial.forEach(e -> {
-                Instance instance = new Instance(2);
-                instance.setValue((Attribute) atts.elementAt(0), historial.indexOf(e));
-                instance.setValue((Attribute) atts.elementAt(1), e.getDecibeles());
-                dataset.add(instance);
-            });*/
-            FastV fv = new FastV(historial.size());
-
-            historial.forEach(item -> {
-                fv.addPair(historial.indexOf(item), (int) item.getDecibeles());
-            });
-            double[] coefficients = fv.getFunction();
-            return "{\"m\":" + coefficients[0] + ",\"c\":" + coefficients[2] + "}";
+                historial.forEach(item -> {
+                    fv.addPair(historial.indexOf(item), (int) item.getDecibeles());
+                });
+                double[] coefficients = fv.getFunction();
+                return "{\"m\":" + coefficients[0] + ",\"c\":" + coefficients[2] + "}";
+            }
         });
 
         get("/api/sensors", (req, res) -> {
--- src/main/java/com/edu/udec/aeropuerto/math/FastV.java
+++ src/main/java/com/edu/udec/aeropuerto/math/FastV.java
@@ -1,28 +1,37 @@
 package com.edu.udec.aeropuerto.math;
 
+import weka.associations.Apriori;
 import weka.classifiers.functions.LinearRegression;
 import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 
+import java.util.Enumeration;
+
 /**
- *
  * @author Estudiante
  */
 public class FastV {
     FastVector fv;
     Instances data;
 
-     public FastV(int cantidad) {
+    public FastV(int cantidad) {
         this.fv = new FastVector();
         this.fv.addElement(new Attribute("X"));
         this.fv.addElement(new Attribute("Y"));
-                
+
         data = new Instances("datos", fv, cantidad);
         data.setClassIndex(data.numAttributes() - 1);
     }
-    
+
+    public FastV(int cantidad, Attribute yAttr) {
+        this.fv = new FastVector();
+        this.fv.addElement(yAttr);
+
+        data = new Instances("datos", fv, cantidad);
+        data.setClassIndex(data.numAttributes() - 1);
+    }
     public void addPair(int x, int y) {
         Instance temp;
 
@@ -31,14 +40,33 @@ public class FastV {
         temp.setValue((Attribute) fv.elementAt(1), y);
         data.add(temp);
     }
-    
-    public double [] getFunction() throws Exception {
-        
-        LinearRegression lr = new LinearRegression();
-        
-        lr.buildClassifier(data);
-        
-        return lr.coefficients();        
+
+    public void addValue(String y) {
+        Instance temp;
+
+        temp = new Instance(1);
+
+        Attribute att = (Attribute) fv.elementAt(0);
+        Enumeration enumeration = att.enumerateValues();
+        temp.setValue(att, y);
+        data.add(temp);
     }
-    
+
+    public double[] getFunction() throws Exception {
+
+        LinearRegression lr = new LinearRegression();
+
+        lr.buildClassifier(data);
+
+        return lr.coefficients();
+    }
+
+    public Apriori getAssociation() throws Exception {
+
+
+        Apriori apriori = new Apriori();
+        apriori.setClassIndex(data.classIndex());
+        apriori.buildAssociations(data);
+        return apriori;
+    }
 }
